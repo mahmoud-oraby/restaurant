c6de24e54ec41d77f618dc4c0a3b5c179f2e0246
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-08-27 11:14:11 +0300

    add login auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("",views.login,name="login"),
     path("register/",views.register,name="register"),
+    path("done/",views.done,name="done"),
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate,login as auth_login
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 
@@ -24,4 +25,30 @@ def register(request):
 
     return render(request,"html/register.html",{"form":form})
 
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user=authenticate(username=username, password=password)
+
+            if user is not None:
+                auth_login(request,user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("done")
+            else:
+                messages.error(request,"Invalid username or password.")
+
+        else:
+            messages.error(request,"Invalid username or password.")
+
+    form = AuthenticationForm()
+    return render(request=request, template_name="html/login.html",context={'form':form})
+
+
+def done(request):
+    return render(request,'html/done.html')
 
