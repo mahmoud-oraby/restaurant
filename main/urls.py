3804b3cc795cb8b98a061a7a7a888554eb8672bd
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-08-30 14:41:37 +0300

    add templates files

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = "main"
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('menu', views.menu, name='menu'),
+    path('contact', views.contact, name='contact'),
 ]
