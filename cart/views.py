257768bfcca78f3e43cc3646592dd429825bc07a
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-09-12 01:14:28 +0300

    Add new functionality

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from order.models import Order, OrderItem
 from main.models import Menu
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,7 +14,20 @@ def cart(request):
     for item in order_item:
         total_price += item.product.price * item.quantity
 
+    if request.method == "POST":
+        quantity = int(request.POST.get('quantity'))
+        if quantity <= 0:
+            messages.error(request, "Should be bigger than 0!")
+            return redirect('cart:cart')
+        else:
+            item_id = request.POST.get('item_id')
+            item = OrderItem.objects.get(id=item_id)
+            item.quantity = quantity
+            item.save()
+            order_item = OrderItem.objects.filter(order=order).all()
+
     return render(request, 'cart.html', {
         'order_item': order_item,
-        'total_price': str(total_price)
+        'total_price': str(total_price),
+        # 'price_item':
     })
