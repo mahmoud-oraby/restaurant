ae0a87b02084b077a7fa67f33ea60d53fa6c42b9
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-10-18 01:25:44 +0300

    test deploy

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 # from dotenv import load_dotenv
 from pathlib import Path
-import dj_databsae_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_databsae_url.parse('postgres://restaurantdatabase_user:lNegL7hxttny5aANXir2xamLJEc87AmK@dpg-ckdt0tdjhfbs7382ql2g-a.frankfurt-postgres.render.com/restaurantdatabase')
+    'default': dj_database_url.parse('postgres://restaurantdatabase_user:lNegL7hxttny5aANXir2xamLJEc87AmK@dpg-ckdt0tdjhfbs7382ql2g-a.frankfurt-postgres.render.com/restaurantdatabase')
 }
 
 
